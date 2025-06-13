45617d199e3c75109df52c05cd65a2948d31cb76
Saajan Saini <sainisaaji@gmail.com>
2025-06-13 21:35:32 +0200

    tris

--- playground/urls.py
+++ playground/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('play.urls'))
+    path('', include('play.urls')),
+    path('tris/', include('tris.urls'))
 ]
--- tris/views.py
+++ tris/views.py
@@ -4,15 +4,8 @@ from django.template import loader
 
 # Create your views here.
 
-def play(request):
-    template = loader.get_template('play.html')
-    context = {
-
-    }
-    return HttpResponse(template.render(context,request)) 
-
-def home(request):
-    template = loader.get_template('home.html')
+def tris(request):
+    template = loader.get_template('tris.html')
     context = {
 
     }
